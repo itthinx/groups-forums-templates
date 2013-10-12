a47718fdbdb81743dc9cc1b1b065a1ff1104b7f2
kento <kento@phi.(none)>
2013-10-12 23:04:03 +0200

    added default info from base templates using hooks for genesis

--- genesis/groups-forums/single-topic.php
+++ genesis/groups-forums/single-topic.php
@@ -1,3 +1,33 @@
 <?php
-genesis();
+/**
+ * single-topic.php
+ *
+ * This is the default template for Topics (topic custom post type)
+ * when using themes based on the Genesis framework.
+ *
+ * To modify this template:
+ * - Create a groups-forums subfolder in your theme's root folder.
+ * - Copy this file there and adjust it as desired.
+ *
+ * @author itthinx
+ */
 
+add_action( 'genesis_after_post_content', 'gf_genesis_after_post_content' );
+
+function gf_genesis_after_post_content() {
+	$forums = get_the_term_list( get_the_ID(), 'forum', '', ', ', '' );
+	if ( !empty( $forums ) ) {
+		echo '<div class="forums">';
+		echo sprintf( __( 'Posted in %s', GROUPS_FORUMS_PLUGIN_DOMAIN ) , $forums );
+		echo '</div>';
+	}
+
+	$tags = get_the_term_list( get_the_ID(), 'topic_tag', '', ', ', '' );
+	if ( !empty( $tags ) ) {
+		echo '<div class="tags">';
+		echo sprintf( __( 'Tags %s', GROUPS_FORUMS_PLUGIN_DOMAIN ) , $tags );
+		echo '</div>';
+	}
+}
+
+genesis();
